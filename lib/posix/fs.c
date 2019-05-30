ca89a60ae851d5957056c183e9fc27c41e9c12c7
Jim Paris <jim@jtan.com>
2019-05-30 17:00:47 -0400

    Fix readdir() to return NULL on end of directory

--- lib/posix/fs.c
+++ lib/posix/fs.c
@@ -248,6 +248,10 @@ struct dirent *readdir(DIR *dirp)
 		return NULL;
 	}
 
+	if (fdirent.name[0] == 0) {
+		return NULL;
+	}
+
 	rc = strlen(fdirent.name);
 	rc = (rc < MAX_FILE_NAME) ? rc : (MAX_FILE_NAME - 1);
 	(void)memcpy(pdirent.d_name, fdirent.name, rc);
